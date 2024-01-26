e2989802b85ea7d3d87f1c0e8ece9706ccfb22c4
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2024-01-26 13:02:07 +0630

    add dm

--- app/Http/Controllers/Api/QuestionController.php
+++ app/Http/Controllers/Api/QuestionController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use Exception;
+use App\Helpers\ApiResponse;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\QuestionRequest;
-use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
@@ -12,52 +14,70 @@ class QuestionController extends Controller
 
     public function directMethod(QuestionRequest $request)
     {
-        $validatedData = $request->validated();
+        try {
 
+            $validatedData = $request->validated();
 
-        $firstNumber = rand(1, 9);
+            $firstNumber = [];
+            $firstNumber = rand(1, 9);
 
-        $possibleOutcome = [];
-        $questions = [$firstNumber];
+            $possibleOutcome = [];
+            $questions = [$firstNumber];
 
+            for ($i = 0; $i <= $validatedData['line']; $i++) {
 
-        for ($i = 0; $i <= $validatedData['line']; $i++) {
+                switch ($firstNumber) {
+                    case 0:
+                        $possibleOutcome = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+                        break;
+                    case 1:
+                        $possibleOutcome = [1, 2, 3, 5, 6, 7, 8, -1];
+                        break;
+                    case 2:
+                        $possibleOutcome = [1, 2, 5, 6, 7, -1, -2];
+                        break;
+                    case 3:
+                        $possibleOutcome = [1, 5, 6, -1, -2, -3];
+                        break;
 
-            switch ($firstNumber) {
-                case 0:
-                    $possibleOutcome = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-                    break;
-                case 1:
-                    $possibleOutcome = [1, 2, 3, 5, 6, 7, 8, -1];
-                    break;
-                case 2:
-                    $possibleOutcome = [1, 2, 5, 6, 7, -1, -2];
-                    break;
-                case 3:
-                    $possibleOutcome = [1, 5, 6, -1, -2, -3];
-                    break;
+                    case 4:
+                        $possibleOutcome = [5, -1, -2, -3, -4];
+                        break;
 
-                case 4:
-                    $possibleOutcome = [5, -1, -2, -3, -4];
-                    break;
+                    case 5:
+                        $possibleOutcome = [1, 2, 3, 4, -5];
+                        break;
 
-                case 5:
-                    $possibleOutcome = [1, 2, 3, 4, -5];
-                    break;
+                    case 6:
+                        $possibleOutcome = [1, 2, 3, -1, -5, -6];
+                        break;
+                    case 7:
+                        $possibleOutcome = [1, 2, -1, -2, -5, -6, -7];
+                        break;
+                    case 8:
+                        $possibleOutcome = [1, -1, -2, -3, -5, -6, -7, -8];
+                        break;
+                    case 9:
+                        $possibleOutcome = [-1, -2, -3, -4, -5, -6, -7, -8, -9];
+                        break;
+                }
 
-                case 6:
-                    $possibleOutcome = [1, 2, 3, -1, -5, -6];
-                    break;
-                case 7:
-                    $possibleOutcome = [1, 2, -1, -2, -5, -6, -7];
-                    break;
-                case 8:
-                    $possibleOutcome = [1, -1, -2, -3, -5, -6, -7, -8];
-                    break;
-                case 9:
-                    $possibleOutcome = [-1, -2, -3, -4, -5, -6, -7, -8, -9];
-                    break;
+                $nextQuestion = $possibleOutcome[array_rand($possibleOutcome)];
+                $questions[] = $nextQuestion;
+                $firstNumber = array_reduce($questions, function ($a, $b) {
+                    return $a + $b;
+                });
             }
+            $answer = array_sum($questions);
+
+            $responseData = [
+                'questions' => $questions,
+                'answer' => $answer,
+            ];
+
+            return ApiResponse::responseWithSuccess('Direct Method Success!', $responseData, 200);
+        } catch (Exception $e) {
+            return ApiResponse::responseWithBadRequest($e->getMessage());
         }
     }
 }
